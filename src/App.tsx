9ddbf01576d7992c8400b97b8fef6bf507a4baef
Rafael Reis <rafa.araujoreis@gmail.com>
2021-12-18 06:14:03 -0300

    Componentização

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,23 @@ import './styles/global.scss';
 import './styles/sidebar.scss';
 import './styles/content.scss';
 
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
 export function App() {
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
